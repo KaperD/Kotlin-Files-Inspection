19908f3e15966efc9d4c253ef6d8490e8d7dff20
Danil Bubnov <dkbubnov@edu.hse.ru>
2022-02-13 21:47:13 +0300

    Show notification when Kotlin file is opened

---
+++ src/main/kotlin/ru/hse/inspection/icons/Icons.kt
@@ -0,0 +1,8 @@
+package ru.hse.inspection.icons
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object Icons {
+    val KOTLIN: Icon = IconLoader.getIcon("/icons/kotlinLanguage.svg", Icons::class.java)
+}
