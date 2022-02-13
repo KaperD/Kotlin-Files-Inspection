3578b0eca735606996564e4fe1b0b9ba5ce246fa
GitHub Action <action@github.com>
2022-02-13 10:42:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kaperd/kotlinfilesinspection/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kaperd.kotlinfilesinspection.services
+
+import com.github.kaperd.kotlinfilesinspection.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kaperd/kotlinfilesinspection/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kaperd/kotlinfilesinspection/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kaperd.kotlinfilesinspection.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kaperd.kotlinfilesinspection.MyBundle
 
 class MyProjectService(project: Project) {
 
